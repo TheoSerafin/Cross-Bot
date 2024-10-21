4b3a2fc5db531b0883b1918994d0593967c28406
TheoSerafin <theosserafin@gmail.com>
2024-10-21 20:32:56 -0300

    Update Nim.cpp

--- Nim.cpp
+++ Nim.cpp
@@ -1,10 +1,10 @@
-#include <iostream>
-#include <fstream>
-#include <cstdlib>
+#include <iostream>//Biblioteca padr√£o do c++
+#include <fstream>//Biblioteca para usar arquivos txt
+#include <cstdlib>//Biblioteca para gerar "n√∫meros" aleat√≥rios 
 
 using namespace std;
 
-void Tabunim(int *npalitos){ //cria o mÈtodo que imprime o tabuleiro do Nim no terminal
+void Tabunim(int *npalitos){ //cria o m√©todo que imprime o tabuleiro do Nim no terminal
 
 for(int loop = 0;loop<*npalitos;loop++){ //imprime a quantidade de palitos que foi escolhido
    cout<<"| ";
@@ -12,36 +12,36 @@ for(int loop = 0;loop<*npalitos;loop++){ //imprime a quantidade de palitos que f
 cout<<"\n"<<"Numero de palitos restantes: "<<*npalitos<<"\n";
 }
 ///////////////////////////////////////////////////////////////////////////////////
-void jogador1(int *npalitos){ //os mÈtodos para o jogador 1
-int tpalitos; //recebe a quantidade de palitos que v„o ser retirados
+void jogador1(int *npalitos){ //os m√©todos para o jogador 1
+int tpalitos; //recebe a quantidade de palitos que v√£o ser retirados
 
 cout<<"Digite quantos palitos pegar (1,2 ou 3): ";
 cin>>tpalitos;
-if(tpalitos >0 && tpalitos<=3){ //verifica se a quantidade de palitos que v„o ser retirados est· entre (1,2 ou 3)
- if(tpalitos <= *npalitos){ //redund‚ncia para que o n˙mero de palitos que v„o ser retirados n„o seja maior que o a quantidade palitos que h· no jogo
+if(tpalitos >0 && tpalitos<=3){ //verifica se a quantidade de palitos que v√£o ser retirados est√° entre (1,2 ou 3)
+ if(tpalitos <= *npalitos){ //redund√¢ncia para que o n√∫mero de palitos que v√£o ser retirados n√£o seja maior que o a quantidade palitos que h√° no jogo
    *npalitos=*npalitos-tpalitos; //retira os palitos
  }else{ //caso o numero de palitos retirado seja maior que a quantidade de palitos que sobraram no jogo
    cout<<"\n"<<"Quantidade indevida, sobrou apenas "<<*npalitos<<" palito(s) no jogo"<<"\n"<<"\n";
-   Tabunim(npalitos); //mostra de novo o tabuleiro pro jogador n„o se perder
-   jogador1(npalitos); //funÁ„o recursiva
+   Tabunim(npalitos); //mostra de novo o tabuleiro pro jogador n√£o se perder
+   jogador1(npalitos); //fun√ß√£o recursiva
  }
-}else{ //caso o n˙mero de palitos retirados n„o esteja no intervalo [1,3]
+}else{ //caso o n√∫mero de palitos retirados n√£o esteja no intervalo [1,3]
    cout<<"\n"<<"Quantidade invalida, pode retirar apenas (1,2 ou 3) palitos de uma vez "<<"\n"<<"\n";
-   Tabunim(npalitos); //mostra de novo o tabuleiro pro jogador n„o se perder
-   jogador1(npalitos); //funÁ„o recursiva
+   Tabunim(npalitos); //mostra de novo o tabuleiro pro jogador n√£o se perder
+   jogador1(npalitos); //fun√ß√£o recursiva
 }
 }
 ///////////////////////////////////////////////////////////////////////////////////
-void bot(int *npalitos){//os mÈtodos para o bot
+void bot(int *npalitos){//os m√©todos para o bot
 
-if(*npalitos%4 == 0){//verifica se a quantidade de palitos que sobrou da vez do jogador1 È multiplo de 4
-   int aleatorio = rand() % 3 + 1;//Pega um n˙mero aleatÛrio entre 1 e 3
-   *npalitos = *npalitos - aleatorio; //tira uma quantidade aleatÛria de palitos
+if(*npalitos%4 == 0){//verifica se a quantidade de palitos que sobrou da vez do jogador1 √© multiplo de 4
+   int aleatorio = rand() % 3 + 1;//Pega um n√∫mero aleat√≥rio entre 1 e 3
+   *npalitos = *npalitos - aleatorio; //tira uma quantidade aleat√≥ria de palitos
    cout<<"O computador pegou "<<aleatorio<<" palito(s)"<<"\n";
-}else{ //verifica quando a quantidade de palito que sobrou da vez do jogador1 n„o È multiplo de 4
+}else{ //verifica quando a quantidade de palito que sobrou da vez do jogador1 n√£o √© multiplo de 4
    int loop = 0;
 
-if(*npalitos>4){ //verifica se h· mais de 4 palitos
+if(*npalitos>4){ //verifica se h√° mais de 4 palitos
    while((*npalitos-loop)%4 != 0){ //verifica quantos palitos precisa tirar para sobrar um multiplo de 4
    loop++;
 }
@@ -55,7 +55,7 @@ cout<<"O computador pegou "<<loop<<" palito(s)"<<"\n";
 }
 }
 ///////////////////////////////////////////////////////////////////////////////////
-void historico(string JogoNim){ // Essa funÁ„o guarda o resultado de todas as partidas
+void historico(string JogoNim){ // Essa fun√ß√£o guarda o resultado de todas as partidas
 ofstream arquivo;// cria um arquivo txt para guarda os resultados
 arquivo.open("nim.txt", ios::app);
 arquivo<<JogoNim<<"\n"; //imprime no arquivo o resultado da partida
@@ -64,9 +64,9 @@ arquivo.close(); //fecha o arquivo
 ifstream arquivo1; //abre o aquivo txt para ler os textos
 arquivo1.open("nim.txt");
 if(arquivo1.is_open()){ //verifica se foi possivel abrir o arquivo
-int bot = 0;int jg1 = 0; //  Vari·veis para contar quantas partidas o Jogador1 e o Bot venceram
+int bot = 0;int jg1 = 0; //  Vari√°veis para contar quantas partidas o Jogador1 e o Bot venceram
 string linha; //variavel que recebe o texto das linhas
-while(getline(arquivo1,linha)){ //lÍ as linhas do arquivo
+while(getline(arquivo1,linha)){ //l√™ as linhas do arquivo
 if(linha == "Jogador1"){ //verifica quem ganhou naquela linha e adiciona na variavel correta
   jg1++;
 }else{
@@ -74,46 +74,46 @@ if(linha == "Jogador1"){ //verifica quem ganhou naquela linha e adiciona na vari
 }
 }
 cout<<"\n\nPlacar: "<<"Jogador:"<<jg1<<" X "<<"Bot:"<<bot; //imprime o placar
-}else{ //caso n„o consiga abrir o arquivo essa funÁ„o n„o faz nada
+}else{ //caso n√£o consiga abrir o arquivo essa fun√ß√£o n√£o faz nada
     return;
 }
 }
 ///////////////////////////////////////////////////////////////////////////////////
- int JogoNim(int *npalitos) { //Junta todos os mÈtodos numa funÁ„o sÛ
+ int JogoNim(int *npalitos) { //Junta todos os m√©todos numa fun√ß√£o s√≥
  Tabunim(npalitos); //inicia o tabuleiro
- jogador1(npalitos); //Inicia o mÈtodo do jogador
- if(*npalitos==0){ //Verifica-se o jogador j· venceu
+ jogador1(npalitos); //Inicia o m√©todo do jogador
+ if(*npalitos==0){ //Verifica-se o jogador j√° venceu
     cout<<"Fim de jogo -> O Jogador1 Venceu ";
     historico("Jogador1"); //escreve Jogador1 no arquivo txt
     return 0;
  }
  Tabunim(npalitos); //atualiza o tabuleiro
- bot(npalitos); //inicia o mÈtodo do bot
- if(*npalitos==0){ //Verifica-se o Bot j· venceu
+ bot(npalitos); //inicia o m√©todo do bot
+ if(*npalitos==0){ //Verifica-se o Bot j√° venceu
     cout<<"Fim de jogo -> O Computador Venceu ";
     historico("Computador"); //escreve computador no arquivo txt
     return 0;
  }
- JogoNim(npalitos);//funÁ„o recursiva caso o jogo ainda n„o tenha terminado
+ JogoNim(npalitos);//fun√ß√£o recursiva caso o jogo ainda n√£o tenha terminado
  }
 ///////////////////////////////////////////////////////////////////////////////////
 int main(){
 int npalitos = 0; //variavel que recebe a quantidade de palitos do jogo
-cout<<"Digite a quantidade inicial de palitos: "; //pergunta ao jogador o n˙mero de palitos
+cout<<"Digite a quantidade inicial de palitos: "; //pergunta ao jogador o n√∫mero de palitos
 cin>>npalitos; cout<<"\n";
-while (npalitos <= 0) {  //redund‚ncia para que a quantidade de palitos necessaria pra comeÁar o jogo seja >0
+while (npalitos <= 0) {  //redund√¢ncia para que a quantidade de palitos necessaria pra come√ßar o jogo seja >0
   cout <<"Quantidade invalida, digite um numero > 0 para iniciar o jogo"<<"\n";
   cout<<"Digite a quantidade inicial de palitos: ";
   cin >> npalitos;  cout<<"\n";
 }
-JogoNim(&npalitos); //chama a funÁ„o JogoNIm
+JogoNim(&npalitos); //chama a fun√ß√£o JogoNIm
 
 string reiniciar;
 cout<<"\n"<<"\n"<<"Jogar Novamente?(s/n) ";
 cin>>reiniciar;
-if(reiniciar == "s" || reiniciar == "S"){ //da a opÁ„o do jogador reiniciar o jogo sem ter que fechar o terminal
+if(reiniciar == "s" || reiniciar == "S"){ //da a op√ß√£o do jogador reiniciar o jogo sem ter que fechar o terminal
    system("cls"); //limpa o terminal, funciona apenas em Windows, Apple ou Linux usar system("clear")
-   main(); //funÁ„o recursiva
+   main(); //fun√ß√£o recursiva
 }
 return 0;
 }
